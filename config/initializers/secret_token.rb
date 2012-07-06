ea11d6de5f253f6359806631f22c0f0a4cc467f7
Chris Allen Moore <igotmusic@gmail.com>
2012-07-06 13:44:22 +0700

    Intitial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seomarketing::Application.config.secret_token = '7329d79f4cd75af9a5417ab2b3f0e5af7f45f91a4328a75ec4607b4856887f8f56e52efd1741adc326781f9d4b2074f5f8606b26c5073fbd6951204a094dbb70'
