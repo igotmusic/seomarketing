ae132a16eb418efac88ff6b564991493e60792a7
Chris Moore <igotmusic@gmail.com>
2012-07-27 12:21:46 +0700

    initial commit

--- config/initializers/copycopter.rb
+++ config/initializers/copycopter.rb
@@ -1,4 +1,4 @@
 CopycopterClient.configure do |config|
   config.api_key = '6ce413876cd435d116bca6ae0cf0d238d7106395ae336d04'
-  config.host = 'mysterious-garden-7980.herokuapp.com'
+  config.host = 'copycopter-webexperts.herokuapp.com'
 end
